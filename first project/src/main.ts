48a3a02c46ee195b0058e62e061917068f7a6fdc
Luca <luca.weimer.42@gmail.com>
2025-05-28 21:34:08 +0200

    Test

--- first project/src/main.ts
+++ first project/src/main.ts
@@ -1,41 +1,9 @@
-import './css/style.css'
 import PlayerLeaderboardList from './model/FullList'
-import ListItem from './model/ListItem'
+import ListItem from './model/PlayerItem'
 import ListTemplate from './template/ListTemplate'
 
 const initApp = (): void => {
-    const fullList = PlayerLeaderboardList.instance;
-    const template = ListTemplate.instance;
-
-    const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement;
-    itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
-        event.preventDefault();
-        
-        fullList.list.reverse();
-        const input = document.getElementById("newItem") as HTMLInputElement;
-        const newEntryText: string = input.value.trim();
-        if(!newEntryText) return;
-
-        const itemId: number = fullList.list.length
-         ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
-         : 1
-        
-        const newItem = new ListItem(itemId.toString(), newEntryText, false);
-
-        fullList.addItem(newItem);
-        input.value = "";
-        fullList.list.reverse();
-        template.render(fullList);
-    })
-
-    const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement;
-    clearItems.addEventListener("click", ():void => {
-        fullList.clear();
-        template.clear();
-    })
-
-    fullList.load();
-    template.render(fullList);
+    
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
